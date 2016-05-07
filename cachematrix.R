cfdf70a0ea19196536e78bbc2f66ce4ae097909c
Francis McGuire <fr@ncismcguire.com>
2016-05-07 14:53:08 -0400

    Completed exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache allow to run Solve on a matrix,
+## If said matrix has already been solved, the result is fetched
+## from the cache
 
-## Write a short comment describing this function
-
+## Creates an cache object intented for a matrix solve
+## Has access and set methods
+## Returns a list of functions
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## tries to retrieve a matrix from the passed cache object
+## if no solve exists, it runs the solve on said object
+## Once solve is complete, results are cached to the cache object
+## Returns a solved matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
